3ef8beebfa212ad1076d6e7b7f855d0d85b6275e
RicardopcJunior <ricardo.paulo.cjr@hotmail.com>
2026-06-18 12:25:27 -0300

    .

--- java.js
+++ java.js
@@ -1,1 +1,38 @@
- 
+const carrinho = []
+
+
+
+
+function soma_ingresso(lista){
+    let soma =0;
+    for(let i = 0; i< lista.length; i++){
+        soma += lista[i].preco;
+    }
+
+    return soma;
+}
+
+function mostrar_carrinho(){
+    for(let i = 0; i< carrinho.length; i++){  
+    }
+}
+
+function mais_caro(lista){
+    let caro = lista[0];
+    for(let i= 1; i<lista.length; i++){
+        if(lista[i].preco >caro.preco){
+            caro= lista[i];
+        }
+    }
+    return caro;
+}
+
+function mais_barato(lista){
+    let barato = lista[0];
+    for(let i = 1; i<lista.length; i++){
+        if(lista[i].preco <barato.preco){
+             barato = lista[i];
+        }
+    }
+    return barato;
+}
