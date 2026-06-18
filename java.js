c706f9b77410377eb14a70bb0cbae8dca677a10b
RicardopcJunior <ricardo.paulo.cjr@hotmail.com>
2026-06-18 14:59:47 -0300

    quase no fim

--- java.js
+++ java.js
@@ -1,7 +1,41 @@
 const carrinho = []
+const listaCarrinho = document.getElementById("lista-carrinho");
+const total = document.getElementById("total");
+const botoes = document.querySelectorAll(".botao_tabela");
+const conteudo= document.querySelector('.conteudo');
 
+for (let i = 0; i < botoes.length; i++) {
 
+    botoes[i].addEventListener("click", function() {
 
+        carrinho.push({
+            ingresso: this.dataset.ingresso,
+            preco: Number(this.dataset.valor)
+        });
+
+        mostrar_carrinho();
+        console.log(carrinho);
+    });
+
+}
+
+function mostrar_carrinho(){
+
+    listaCarrinho.innerHTML = "";
+
+    for(let i = 0; i < carrinho.length; i++){
+
+        listaCarrinho.innerHTML += `
+            <p>
+                ${carrinho[i].ingresso}
+                - R$ ${carrinho[i].preco}
+            </p>
+        `;
+    }
+
+    total.textContent =
+        `Total: R$ ${soma_ingresso(carrinho).toFixed(2)}`;
+}
 
 function soma_ingresso(lista){
     let soma =0;
@@ -12,10 +46,6 @@ function soma_ingresso(lista){
     return soma;
 }
 
-function mostrar_carrinho(){
-    for(let i = 0; i< carrinho.length; i++){  
-    }
-}
 
 function mais_caro(lista){
     let caro = lista[0];
@@ -36,3 +66,11 @@ function mais_barato(lista){
     }
     return barato;
 }
+
+const menucarrinho = document.querySelector(".menu-carrinho");
+const cart = document.querySelector(".cart");
+
+cart.addEventListener('click', ()=>{
+    menucarrinho.classList.toggle("ativo");
+     conteudo.classList.toggle('ativo');
+});
